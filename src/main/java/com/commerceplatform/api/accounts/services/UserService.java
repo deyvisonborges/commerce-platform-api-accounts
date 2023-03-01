a354d8d3fd23b4f0bcb968454b8ab80d736226f5
deyvisonborges <deyv.borges@gmail.com>
2023-03-01 14:56:07 -0300

    feat: adjust in create user service

--- src/main/java/com/commerceplatform/api/accounts/services/UserService.java
+++ src/main/java/com/commerceplatform/api/accounts/services/UserService.java
@@ -3,31 +3,41 @@ package com.commerceplatform.api.accounts.services;
 import com.commerceplatform.api.accounts.dtos.UserDTO;
 import com.commerceplatform.api.accounts.dtos.mappers.UserMapper;
 import com.commerceplatform.api.accounts.exceptions.BadRequestException;
-import com.commerceplatform.api.accounts.exceptions.NotFoundException;
 import com.commerceplatform.api.accounts.models.UserModel;
 import com.commerceplatform.api.accounts.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public UserModel createUser(UserDTO userDto) {
         try {
             if(Objects.isNull(userDto.getId())) {
-                throw new BadRequestException("Id deve ser nulo");
+                throw new BadRequestException("Id must be null");
             }
 
+            var existsEmail = userRepository.findByEmail(userDto.getEmail());
+            if (existsEmail.isPresent())
+                throw new BadRequestException("Email already registered");
+
             var newUser = UserMapper.mapper(userDto);
+             newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
             return userRepository.save(newUser);
         } catch (Exception e) {
-            throw new NotFoundException("Não foi possível criar o usuário");
+            throw new BadRequestException("Failed to create user: "+e.getMessage());
         }
     }
 }
