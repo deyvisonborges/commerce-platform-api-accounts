d6e99ef6421e83326a318a54d4f9f00f66c8fbcc
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-04-18 13:48:32 -0300

    resolve validators null

--- src/main/java/com/commerceplatform/api/accounts/repositories/jpa/RoleRepository.java
+++ src/main/java/com/commerceplatform/api/accounts/repositories/jpa/RoleRepository.java
@@ -6,13 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleModel, Long> {
-
-//    @Query("SELECT r FROM RoleModel r WHERE r.name = ?1")
-//    RoleModel findByName(String name);
-
     @Query("SELECT r FROM RoleModel r WHERE LOWER(r.name) = LOWER(?1)")
-    List<RoleModel> findByName(String name);
+    Optional<List<RoleModel>> findByName(String name);
 }
